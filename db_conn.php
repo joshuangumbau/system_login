3158872890d1b4d10d2d84a5d720df868bf01274
ngumbaujoshua4@gmail.com <joshie ngumbau>
2022-02-21 15:29:02 +0300

    all changes done today

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,12 @@
 <?php
 
 $sname= "localhost";
-$unmae= "root";
+$uname= "root";
 $password = "";
 
-$db_name = "test_db";
+$db_name = "login_db";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
 	echo "Connection failed!";
